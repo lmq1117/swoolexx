6cd45240c198635d384687a90ae28beb72c81bd0
mingquan.li <mingquan.li@5262.com>
2017-09-06 17:26:15 +0800

    Client敲完了

---
+++ Client.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 2017/9/6
+ * Time: 17:15
+ */
+class Client
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+    }
+
+    public function connect()
+    {
+        if (!$this->client->connnect("127.0.0.1",9501,1))
+        {
+            echo "Error:{$fp->errMsg}[{$fp->errCode}]\n";
+        }
+
+        fwrite(STDOUT,"请输入消息：");
+        $msg = trim(fgets(STDIN));
+        $this->client->send($msg);
+
+        $message = $this->client->recv();
+        echo "Get Message From Server:{$message}\n";
+    }
+
+    public function test(){
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+    }
+}
+
+$client = new Client();
+$client->connect();
