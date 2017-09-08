9f71d5d7cc341c238efc624a2b4d2af52f4d24c0
mingquan.li <mingquan.li@5262.com>
2017-09-08 10:41:56 +0800

    swoole 简易聊天服务端

---
+++ liaotianshi/swoole_websocket_server.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 2017/9/8
+ * Time: 10:18
+ */
+$server = new swoole_websocket_server("120.24.19.13",9886);
+
+$server->on('open','onOpen');
+$server->on('message','onMessage');
+$server->on('close','onClose');
+
+
+function onOpen(swoole_websocket_server $serv,$request){
+    echo "[服务端]:握手成功，客户端id是:{$request->fd}\n";//$request->fd 是客户端id
+}
+
+/**
+ * @param swoole_websocket_server $serv
+ * @param $frame
+ */
+function onMessage(swoole_websocket_server $serv,$frame){
+    //$frame->fd 是客户端id，$frame->data是客户端发送的数据
+    //服务端向客户端发送数据是用 $server->push( '客户端id' ,  '内容')
+    echo "收到来自客户端id是--{$frame->fd}--的消息:内容是--{$frame->data}--,opcode:--{$frame->opcode}--,fin:--{$frame->finish}--\n";
+    $serv->push($frame->fd,'我是服务器，我收到你消息了，朕知道了，阅。');
+}
+
+function onClose($serv,$fd){
+    echo "客户端id为 --{$fd}-- 闪人了！";
+}
+
+$server->start();
