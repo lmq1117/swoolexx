d271260f01f02cf0c0319385d35f17185f6c9945
mingquan.li <mingquan.li@5262.com>
2017-10-25 11:00:33 +0800

    mysql连接池演示task进程有什么用v1

---
+++ taskworkzuoyong/MySQLPool.php
@@ -0,0 +1,77 @@
+<?php
+class MySQLPool
+{
+    private $serv;
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->serv = new swoole_server("0.0.0.0",9501);
+
+        $this->serv->set([
+            'worker_num'=>8,
+            'daemonize'=>false,
+            'max_request'=>10000,
+            'dispatch_mode'=>3,
+            'debug_mode'=>1,
+            'task_worker_num'=>8,
+        ]);
+
+        $this->serv->on('WorkerStart',[$this,'onWorkerStart']);
+        $this->serv->on('Connect',[$this,'onConnect']);
+        $this->serv->on('Receive',[$this,'onReceive']);
+        $this->serv->on('Close',[$this,'onClose']);
+
+        //bind callback
+        $this->serv->on('Task',[$this,'onTask']);
+        $this->serv->on('Finish',[$this,'onFinish']);
+
+        $this->serv->start();
+    }
+
+    public function onConnect( $serv, $fd, $from_id )
+    {
+        echo "Client {$fd} connect\n";
+    }
+
+    public function onClose( $serv, $fd, $from_id )
+    {
+        echo "Client {$fd} close connection\n";
+    }
+
+    public function onWorkerStart()
+    {
+        echo "onWorkerStart\n";
+        $this->pdo = new PDO(
+            "mysql:localhost;port=3306;dbname=Test",
+            'root',
+            '123456',
+            [
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_PERSISTENT => true,
+            ]
+        );
+    }
+
+    public function onReceive(swoole_server $serv, $fd, $from_id, $data )
+    {
+
+    }
+
+    public function onTask( $serv, $task_id, $from_id, $data )
+    {
+        try {
+            return true;
+        } catch ( PDOException $e ) {
+            var_dump($e);
+            return false;
+        }
+    }
+
+    public function onFinish( $serv, $task_id, $data)
+    {
+        var_dump("result: " . $data);
+    }
+}
+new MySQLPool();
